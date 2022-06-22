adccba8bc4402cc3e2a25f2ef3d0bd609b99187c
haruki <soccer_haruki15@gmail.com>
2022-06-22 15:16:10 -0700

    initial commit

---
+++ src/main/java/com/fullstack/studentfullstack/StudentFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.studentfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentFullstackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentFullstackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fullstack/studentfullstack/StudentFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.studentfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentFullstackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
