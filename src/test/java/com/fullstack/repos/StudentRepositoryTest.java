6dbbf628e22120839a380ee7c5e42240392a4c10
haruki <soccer_haruki15@gmail.com>
2022-06-24 14:41:41 -0700

    Change database config bean condition.

--- src/test/java/com/fullstack/repos/StudentRepositoryTest.java
+++ src/test/java/com/fullstack/repos/StudentRepositoryTest.java
@@ -6,11 +6,15 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 @DataJpaTest
+@TestPropertySource(
+        locations = "classpath:application-test.properties"
+)
 class StudentRepositoryTest {
 
     @Autowired
