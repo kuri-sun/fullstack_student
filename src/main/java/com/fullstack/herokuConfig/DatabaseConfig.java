5a73e5be13863e9ba5cfc323d669e3021c332c9c
haruki <soccer_haruki15@gmail.com>
2022-06-29 23:44:51 -0700

    added hikari database in heroku

--- src/main/java/com/fullstack/herokuConfig/DatabaseConfig.java
+++ src/main/java/com/fullstack/herokuConfig/DatabaseConfig.java
@@ -8,16 +8,16 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
-//@Configuration
-//public class DatabaseConfig {
-//
-//    @Value("${spring.datasource.url}")
-//    private String dbUrl;
-//
-//    @Bean
-//    public DataSource dataSource() {
-//        HikariConfig config = new HikariConfig();
-//        config.setJdbcUrl(dbUrl);
-//        return new HikariDataSource(config);
-//    }
-//}
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
+}
